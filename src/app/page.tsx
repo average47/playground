bb227e7d2c6055f05c35523ced1ce412bb15e396
Ryan A. Moore <ryan.moore@amcnetworks.com>
2025-07-16 15:21:22 -0400

    progress ring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import { Progress } from '@/components/Progress';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Progress strokeColor="#FF9F1E" />
+        <Progress
+          strokeColor="#FF9F1E"
+          length={5}
+          size={24}
+        />
       </main>
     </div>
   );
