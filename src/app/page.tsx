537da44386ccedd551fdde5d2a7f922dafeb6285
Ryan A. Moore <ryan.moore@amcnetworks.com>
2025-12-08 22:42:34 -0500

    carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,76 @@
 import { useState, useEffect, useRef, Fragment } from 'react';
 
 export default function Home() {
-  const el = (sel, par) => (par || document).querySelector(sel);
-  const els = (sel, par) => (par || document).querySelectorAll(sel);
-  const elNew = (tag, prop) => Object.assign(document.createElement(tag), prop);
+  const animation = 300;
+  const visibleSlides = 1;
 
-  // Helper functions:
+  const moveDistance = 100 / visibleSlides;
+  const elCarouselSlider = useRef<HTMLDivElement>(null);
+  const elsSlides = useRef<HTMLDivElement[]>([]);
 
-  const mod = (n, m) => ((n % m) + m) % m;
+  let tot = elsSlides.current.length;
+  let c = 0;
+
+  const mod = (n: number, m: number): number => ((n % m) + m) % m;
+
+  const anim = (ms = animation) => {
+    if (!elCarouselSlider.current || !elsSlides.current.length) return;
+    const cMod = mod(c, tot);
+    console.log(animation);
+    // Move slider
+    elCarouselSlider.current.style.transitionDuration = `${ms}ms`;
+    elCarouselSlider.current.style.transform = `translateX(calc(${
+      (-c - visibleSlides) * moveDistance
+    }%))`;
+    // Handle active classes (slide and bullet)
+    elsSlides.current.forEach((elSlide, i) =>
+      elSlide.classList.toggle('is-active', cMod === i)
+    );
+  };
+
   const prev = () => {
     console.log('prev clicked');
-    // if (c <= -1) return; // prevent blanks on fast prev-click
-    // c -= 1;
-    // anim();
+    if (c <= -1) return; // prevent blanks on fast prev-click
+    c -= 1;
+    anim();
   };
 
   const next = () => {
     console.log('next clicked');
-    // if (c >= tot) return; // prevent blanks on fast next-click
-    // c += 1;
-    // anim();
+    if (c >= tot) return; // prevent blanks on fast next-click
+    c += 1;
+    anim();
   };
+
+  // Infinite slide effect:
+  elCarouselSlider.addEventListener('transitionend', () => {
+    if (c <= -1) c = tot - 1;
+    if (c >= tot) c = 0;
+    anim(0); // quickly switch to "c" slide (with animation duration 0)
+  });
+
+  // Clone N first and last slides (for "infinite" slider effect)
+  for (let i = 0; i < visibleSlides; i++) {
+    elCarouselSlider.prepend(elsSlides[tot - 1 - i].cloneNode(true));
+    elCarouselSlider.append(elsSlides[i].cloneNode(true));
+  }
+
+  // Initial slide
+  anim(0);
   return (
     <div
       className="relative overflow-hidden"
       style={{ '--visible-slides': 3 } as React.CSSProperties}>
-      <div className="flex">
+      <div
+        ref={elCarouselSlider}
+        className="flex">
         {Array.from({ length: 10 }, (_, index) => (
           <Fragment key={index}>
-            <div className="aspect-[4/1] bg-gray-800 text-white grow shrink-0 basis-full">
+            <div
+              ref={(element) => {
+                if (element) elsSlides.current[index] = element;
+              }}
+              className="aspect-[4/1] bg-gray-800 text-white grow shrink-0 basis-full">
               <p>Slide {index + 1}</p>
             </div>
           </Fragment>
@@ -52,28 +94,5 @@ export default function Home() {
     </div>
   );
 }
-
-// <div className="bg-amber-300 w-screen h-auto grid grid-cols-[5fr_1fr] gap-2">
-//       <div className="bg-amber-700 aspect-video w-full col-start-1 col-end-2 overflow-scroll flex">
-//         {data.children.map((item: any, idx: number) => (
-//           <div
-//             key={idx}
-//             style={{ backgroundColor: item.color }}
-//             className="w-full flex shrink-0 basis-full">
-//             This is item number {idx + 1}
-//           </div>
-//         ))}
-//       </div>
-//       <ul className="flex flex-col gap-2 w-full col-start-2 col-end-3 overflow-scroll">
-//         {data.children.map((item: any, idx: number) => (
-//           <li
-//             key={idx}
-//             style={{ backgroundColor: item.color }}
-//             className="bg-amber-500 block w-full">
-//             This is item number {idx + 1}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
 
 // https://jsfiddle.net/21fx6ge7/
