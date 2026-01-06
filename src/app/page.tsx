b1387ad9a5814a3313de7ce49ceccf2fde289907
Ryan A. Moore <ryan.moore@amcnetworks.com>
2026-01-06 13:01:58 -0500

    list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,23 @@ export default function Home() {
       visibleSlides: 1,
     },
     children: [
-      { color: '#DCD494' },
-      { color: '#D1805F' },
-      { color: '#005871' },
-      { color: '#854C65' },
-      { color: '#EFDADD' },
-      { color: '#BE394F' },
-      { color: '#704A49' },
-      { color: '#8F3D37' },
-      { color: '#DC343B' },
-      { color: '#82643E' },
-      { color: '#58423F' },
-      { color: '#BDBEBF' },
-      { color: '#464B65' },
-      { color: '#5B3644' },
+      { key: 0, label: 'Branding', color: '#DCD494' },
+      { key: 1, label: 'Identity Design', color: '#D1805F' },
+      { key: 2, label: 'Web Development', color: '#005871' },
+      { key: 3, label: 'Social Media Setup', color: '#854C65' },
+      { key: 4, label: 'Email Marketing', color: '#EFDADD' },
+      { key: 5, label: 'Brochure Development', color: '#BE394F' },
+      { key: 6, label: 'Logo Design', color: '#704A49' },
     ],
   };
+  const lastTwo = data.children.splice(-2);
+  data.children.unshift(...lastTwo);
+  console.log('data', data);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {/* <Progress strokeColor="#FF9F1E" /> */}
-        <List />
+        <List data={data} />
       </main>
     </div>
   );
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -9,21 +9,33 @@ import {
 } from 'react';
 
 const items = [
-  'Branding',
-  'Identity Design',
-  'Web Development',
-  'Social Media Setup',
-  'Email Marketing',
-  'Brochure Development',
-  'Logo Design',
+  { key: 0, label: 'Branding' },
+  { key: 1, label: 'Identity Design' },
+  { key: 2, label: 'Web Development' },
+  { key: 3, label: 'Social Media Setup' },
+  { key: 4, label: 'Email Marketing' },
+  { key: 5, label: 'Brochure Development' },
+  { key: 6, label: 'Logo Design' },
 ];
 
-export default function List() {
+interface ListProps {
+  data: {
+    properties: {
+      autoPlay: boolean;
+      speed: number;
+      visibleSlides: number;
+    };
+    children: { key: number; label: string; color: string }[];
+  };
+}
+export default function List({ data }: ListProps) {
   const [direction, setDirection] = useState(-1);
   const [index, setIndex] = useState(0);
   const [inTransition, setInTransition] = useState(false);
   const container = useRef<HTMLUListElement>(null);
   const [height, setHeight] = useState<number>(0);
+
+  console.log(data);
 
   const windowSizeHandler = () => {
     if (!container.current) return;
@@ -35,10 +47,10 @@ export default function List() {
     setInTransition(false);
     console.log('transition ended');
     if (direction === 1) {
-      container.current.prepend(container.current.lastElementChild!);
+      // container.current.prepend(container.current.lastElementChild!);
     }
     if (direction === -1) {
-      container.current.appendChild(container.current.firstElementChild!);
+      // container.current.appendChild(container.current.firstElementChild!);
     }
   };
 
@@ -63,7 +75,6 @@ export default function List() {
 
   useEffect(() => {
     if (!container.current) return;
-    container.current.prepend(container.current.lastElementChild!);
     setHeight(container.current.children[0].getBoundingClientRect().height);
     window.addEventListener('resize', windowSizeHandler);
     container.current.addEventListener('transitionend', transitionHandler);
@@ -81,17 +92,24 @@ export default function List() {
       <div className="container">
         <div className="carousel w-80 h-60 p-0 m-12 border-10 border-white shadow-md overflow-hidden">
           <ul
+            style={{ transform: `translateY(-${height}px)` }}
             ref={container}
             className="flex flex-col justify-start transition-transform duration-500 ease-in-out">
-            {items.map((item, idx) => (
-              <Fragment key={idx}>
-                <li
-                  data-index={idx}
-                  className="bg-[#f7f7f7] text-[#dddddd] w-full aspect-4/1 transition-colors duration-500 ease-in-out inline-flex items-center justify-center">
-                  {item}
-                </li>
-              </Fragment>
-            ))}
+            {data.children.map(
+              (item: { key: number; label: string; color: string }) => (
+                <Fragment key={item.key}>
+                  <li
+                    data-index={item.key}
+                    className={`w-full aspect-4/1 transition-colors duration-500 ease-in-out inline-flex items-center justify-center ${
+                      item.key === index
+                        ? 'text-[#fff] bg-[#1998c4]'
+                        : 'bg-[#f7f7f7] text-[#dddddd]'
+                    }`}>
+                    {item.label}
+                  </li>
+                </Fragment>
+              )
+            )}
           </ul>
         </div>
       </div>
